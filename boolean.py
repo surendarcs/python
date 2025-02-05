1f7f39f84685b23fbf86dad00c790884226ede0f
Surendar B <152508230+surendarcs@users.noreply.github.com>
2025-02-05 12:33:39 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,21 @@
+'''Boolean Values
+In programming you often need to know if an expression is True or False.
+
+You can evaluate any expression in Python, and get one of two answers, True or False.
+
+When you compare two values, the expression is evaluated and Python returns the Boolean answer'''
+
+########1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+###Print a message based on whether the condition is True or False:
+#########2
+a =200
+b = 100
+if(b>a):
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
