2bec1d6690a8ce2ebf006845a435d8d6dbdf0c51
Surendar B <152508230+surendarcs@users.noreply.github.com>
2025-02-05 17:02:30 +0530

    Add files via upload

---
+++ ilelse.py
@@ -0,0 +1,43 @@
+'''Python Conditions and If statements
+Python supports the usual logical conditions from mathematics:
+
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+These conditions can be used in several ways, most commonly in "if statements" and loops.
+
+An "if statement" is written by using the if keyword.'''
+######1
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+######2
+#Elif - The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+######3 Else - The else keyword catches anything which isn't caught by the preceding conditions.
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+#######4 Nested If -  You can have if statements inside if statements, this is called nested if statements.
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
