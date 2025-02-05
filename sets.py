7fbddd4b4b102367dc3fa7b02b42d6d35a8582f9
Surendar B <152508230+surendarcs@users.noreply.github.com>
2025-02-05 16:43:46 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,11 @@
+'''Set
+Sets are used to store multiple items in a single variable.
+Set is one of 4 built-in data types in Python used to store
+collections of data, the other 3 are List, Tuple, and Dictionary,
+all with different qualities and usage.
+A set is a collection which is unordered, unchangeable*, and unindexed.
+* Note: Set items are unchangeable, but you can remove items and add new items.
+Sets are written with curly brackets.'''
+#######1
+sets = {"apple","banana","orange"}
+print(sets)
